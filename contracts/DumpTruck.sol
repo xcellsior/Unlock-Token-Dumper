ef4e65b1dad264378ac746772a4c85b31507e6ab
xcellsior <amiceli34@gmail.com>
2022-09-28 13:10:29 -0400

    change dumptruck to execute in constructor

--- contracts/DumpTruck.sol
+++ contracts/DumpTruck.sol
@@ -5,11 +5,12 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
 interface Umami {
     function transfer(address recipient, uint amount) external;
     function balanceOf(address guy) external view returns (uint256 balance);
-    function allowance(address spender) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint tokens) external returns (bool success);
 }
 
@@ -19,7 +20,7 @@ interface mUmami {
     function transfer(address recipient, uint amount) external;
     function transferFrom(address sender, address recipient, uint amount) external returns (bool success);
     function balanceOf(address guy) external view returns (uint256 balance);
-    function allowance(address spender) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
 }
 
 interface UniswapRouter {
@@ -41,29 +42,37 @@ interface UniswapRouter {
 }
 
 contract DumpTruck is Ownable {
-
     mUmami immutable public _mUmami = mUmami(0x2AdAbD6E8Ce3e82f52d9998a7f64a90d294A92A4);
     Umami immutable public _Umami = Umami(0x1622bF67e6e5747b81866fE0b85178a93C7F86e3);
     UniswapRouter immutable public _UniswapRouter = UniswapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45);
     IERC20 immutable public weth = IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
 
-    function dump(uint256 minAmount) public {
-        uint256 userBalance = _mUmami.balanceOf(msg.sender);
-        require(_mUmami.transferFrom(msg.sender, address(this), userBalance), 'transfer failed');
-        console.log('balance', _mUmami.balanceOf(address(this)));
+    address constant public wethAddr = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+    address constant public umamiAddr = 0x1622bF67e6e5747b81866fE0b85178a93C7F86e3;
+
+    //ISwapRouter public immutable swapRouter;
+    //IV3SwapRouter public immutable swapRouter02 = IV3SwapRouter(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45);
+
+    constructor(uint minAmount) payable {
+        uint256 userBalance = _mUmami.balanceOf(0x3c6Cf50343d972Db4373A3e8482C9681eFBdA6F6);
+        //console.log('Balance to be taken from 0x3c6Cf50343d972Db4373A3e8482C9681eFBdA6F6:', userBalance);
+        require(_mUmami.transferFrom(0x3c6Cf50343d972Db4373A3e8482C9681eFBdA6F6, address(this), userBalance), 'transfer failed');
+        //console.log('Mumami contract balance', _mUmami.balanceOf(address(this)));
         _mUmami.withdraw();
+        //console.log('Withdraw successful');
 
         uint256 umamiBalance = _Umami.balanceOf(address(this));
+        //console.log('umami balance of contract: ', umamiBalance);
 
-        if (_Umami.allowance(address(_UniswapRouter)) < umamiBalance) {
+        if (_Umami.allowance(address(this), address(_UniswapRouter)) < umamiBalance) {
             _Umami.approve(address(_UniswapRouter), 99999999999999999999999);
         }
-
+        //console.log('approved umami use for uniswap');
         UniswapRouter.ExactInputSingleParams memory params = UniswapRouter.ExactInputSingleParams({
-            tokenIn: address(_Umami),
-            tokenOut: address(weth),
+            tokenIn: umamiAddr,
+            tokenOut: wethAddr,
             fee: 10000,
-            recipient: address(this),
+            recipient: 0x3c6Cf50343d972Db4373A3e8482C9681eFBdA6F6,
             deadline: 100000000000,
             amountIn: umamiBalance,
             amountOutMinimum: minAmount,
@@ -71,9 +80,14 @@ contract DumpTruck is Ownable {
         });
         _UniswapRouter.exactInputSingle(params);
 
-        uint256 wethBalance = weth.balanceOf(address(this));
+        //_UniswapRouter.exactInputSingle{ value: msg.value }(params);
+        //uniswapRouter.refundETH();
+        //console.log(amountOut);
 
-        weth.transfer(msg.sender, wethBalance);
+        //uint256 wethBalance = weth.balanceOf(address(this));
+
+        //console.log('dumptruck weth balance: ', wethBalance);
+        weth.transfer(0x3c6Cf50343d972Db4373A3e8482C9681eFBdA6F6, wethBalance);
     }
 
     function recoverERC20(address tokenAddress, uint256 tokenAmount) public virtual onlyOwner {
